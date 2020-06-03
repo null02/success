89378c7c0439be82624d0ba8c92fcc685084f3de
20971 <20971@DESKTOP-HBBLQAL>
2020-06-03 16:52:26 +0800

    dwfwefwe

--- src/main/java/com/qakj/springboot/App.java
+++ src/main/java/com/qakj/springboot/App.java
@@ -1,5 +1,6 @@
 package com.qakj.springboot;
 
+import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-		System.out.println("sssadasdswqqsqsasda");
+		
+		
+	}
+	@Test
+	public void test1(){
+		
 	}
 }
 
